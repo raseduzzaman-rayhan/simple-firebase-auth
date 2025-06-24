b8edb96a2cbddd237e5725815edcf2d3c2083233
Raseduzzaman Rayhan <raseduzzamanrayhan@gmail.com>
2025-06-24 10:06:59 +0600

    Update firebase.init.js

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -1,25 +1,25 @@
 
-// do not share firebase config online ******* {{******red alert*******}}}
+// // do not share firebase config online ******* {{******red alert*******}}}
 
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyChOsM6n0DKVt8XNgV58G0JHXictw0fUDo",
-  authDomain: "simple-firebase-auth-e4bd9.firebaseapp.com",
-  projectId: "simple-firebase-auth-e4bd9",
-  storageBucket: "simple-firebase-auth-e4bd9.firebasestorage.app",
-  messagingSenderId: "420751247620",
-  appId: "1:420751247620:web:d929b020be88a744a16851"
-};
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyChOsM6n0DKVt8XNgV58G0JHXictw0fUDo",
+//   authDomain: "simple-firebase-auth-e4bd9.firebaseapp.com",
+//   projectId: "simple-firebase-auth-e4bd9",
+//   storageBucket: "simple-firebase-auth-e4bd9.firebasestorage.app",
+//   messagingSenderId: "420751247620",
+//   appId: "1:420751247620:web:d929b020be88a744a16851"
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase Authentication and get a reference to the service
-export const auth = getAuth(app);
+// // Initialize Firebase Authentication and get a reference to the service
+// export const auth = getAuth(app);
